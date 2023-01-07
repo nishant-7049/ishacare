ab1558d66c7ac3db91ad23732b4a399d999f3907
Himanshu Rathore <himanshu.rathore2561@gmail.com>
2023-01-07 16:47:45 +0530

    mychange

--- client/src/components/SliderImg.js
+++ client/src/components/SliderImg.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
 import styled from 'styled-components'
+import Slider from 'react-slick'
 import { MdSchool } from 'react-icons/md'
 import { MdWork } from 'react-icons/md'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 const SliderImg = () => {
   const settings = {
     dots: true,
     infinite: true,
     speed: 200,
-    slidesToShow: 1,
+    slidesToShow: 2,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
@@ -20,24 +20,54 @@ const SliderImg = () => {
   return (
     <Container>
       <Slider {...settings}>
-
         <Items>
-          <div>
-            <h3>Dr. 1 </h3>
-            <div>
+          <div className='disc-contain'>
+            <h3>Dr. Lorem, ipsum. </h3>
+            <div className='disc'>
               <MdWork />
               <p>Lorem ipsum dolor sit amet.</p>
             </div>
-            <div>
+            <div className='disc'>
               <MdSchool />
               <p>Lorem ipsum dolor sit amet.</p>
             </div>
           </div>
-          <div>
+          <div className='doc-img'>
             <img src='/images/doc1.png' alt='' />
           </div>
         </Items>
-        
+        <Items>
+          <div className='disc-contain'>
+            <h3>Dr. Lorem, ipsum. </h3>
+            <div className='disc'>
+              <MdWork />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className='disc'>
+              <MdSchool />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+          </div>
+          <div className='doc-img'>
+            <img src='/images/doc1.png' alt='' />
+          </div>
+        </Items>
+        <Items>
+          <div className='disc-contain'>
+            <h3>Dr. Lorem, ipsum. </h3>
+            <div className='disc'>
+              <MdWork />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className='disc'>
+              <MdSchool />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+          </div>
+          <div className='doc-img'>
+            <img src='/images/doc1.png' alt='' />
+          </div>
+        </Items>
       </Slider>
     </Container>
   )
@@ -46,8 +76,34 @@ const SliderImg = () => {
 export default SliderImg
 
 const Container = styled.div`
-  
+  margin: 2rem 5rem;
+  .slick-prev:before,
+  .slick-next:before {
+    color: black !important;
+  }
 `
 const Items = styled.div`
-  
+  display: flex;
+
+  .disc-contain {
+    max-width: 50%;
+  }
+
+  .disc {
+    display: flex;
+    margin: 1rem;
+  }
+
+  .disc p {
+    margin-left: 0.5rem;
+  }
+
+  .doc-img img {
+    scale: 0.5;
+    width: 100%;
+  }
+
+  .doc-img {
+    max-width: 50%;
+  }
 `

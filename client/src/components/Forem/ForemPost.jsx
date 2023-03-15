682cdfb7d71f77623330951bf9e48d49c772f109
nishant-7049 <nishantsharmay2@gmail.com>
2023-03-15 16:57:09 +0530

    Forem Post changes

--- client/src/components/Forem/ForemPost.jsx
+++ client/src/components/Forem/ForemPost.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import { RxAvatar } from "react-icons/rx";
-import {AiOutlineArrowUp , AiOutlineArrowDown} from "react-icons/ai"
+import {AiOutlineArrowUp , AiOutlineArrowDown, AiFillCloseSquare} from "react-icons/ai"
 import {BiCommentDetail} from "react-icons/bi"
+import Modal from 'react-responsive-modal'
 
 const ForemPost = () => {
+  const [isModelOpen, setIsModelOpen] = useState(false);
+  const close = <AiFillCloseSquare className="text-2xl text-[#f480b1]" />;
   return (
     <div className=" py-3 px-5 mb-[2rem]  bg-white shadow-lg flex flex-col gap-4">
       <div className="flex gap-4 items-center ">
@@ -14,6 +17,21 @@ const ForemPost = () => {
       <div className="flex justify-between">
         <p>This is your question?</p>
         <button className="bg-[#f480b1] px-3 py-2 text-white font-extrabold rounded-3xl">Answer</button>
+        <Modal
+          classNames=" h-[80vh] w-[80vw]"
+          open={isModelOpen}
+          closeIcon={close}
+          onClose={() => setIsModelOpen(false)}
+          closeOnEsccenter
+          closeOnOverlayClick={false}
+          styles={{
+            overlay: {
+              height: "auto",
+            },
+          }}
+        >
+
+        </Modal>
       </div>
       <div className="flex gap-4 items-center">
         <div className="flex gap-4 bg-gray-300 p-2 rounded-3xl">

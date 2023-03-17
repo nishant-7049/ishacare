f2299894eee1496205119324b329942a588d7251
nishant-7049 <nishantsharmay2@gmail.com>
2023-03-17 11:26:25 +0530

    collection/forum and models/Answer,Question changes

--- server/controllers/forum.js
+++ server/controllers/forum.js
@@ -1,8 +1,9 @@
 const QuestionModel = require('../models/Question')
+const AnswerModel = require('../models/Answer')
 const ErrorResponse = require('../utils/errorResponse')
 
 exports.postQuestion = async (req, res, next) => {
-  const { user, question } = req.body
+  const { question, user } = req.body
 
   if (!question || !user) {
     return next(new ErrorResponse('Please provide data', 400))
@@ -15,23 +16,58 @@ exports.postQuestion = async (req, res, next) => {
     })
     res.status(201).send({
       status: true,
-      message: postedQuestion,
+      message: "posted Question",
     })
   } catch (error) {
     next(error)
   }
 }
 
-exports.getQuestions = (req, res, next) => {
-  res.status(200).send({
-    success: true,
-  })
+exports.getQuestions = async (req, res, next) => {
+  try {
+    await QuestionModel.aggregate([
+      {
+        $lookup: {
+          from : "answermodels", 
+          localField : "_id",
+          foreignField : "questionId",
+          as : "allAnswers" 
+        }
+      }
+    ]).exec().then((doc) => {
+      res.status(200).send(doc)
+    }).catch((error) => {
+      res.status(500).send({
+        status: false,
+        message: "unable to get question"
+      })
+    })
+
+  } catch (error) {
+    next(error)
+  }
 }
 
-exports.postAnswer = (req, res, next) => {
-  res.status(200).send({
-    success: true,
-  })
+exports.postAnswer = async (req, res, next) => {
+  const { answer, questionId, user } = req.body
+
+  if (!answer ) {
+    return next(new ErrorResponse('Please provide data', 400))
+  }
+
+  try {
+    const postedAnswer = await AnswerModel.create({
+      user,
+      answer,
+      questionId
+    })
+    res.status(201).send({
+      status: true,
+      message: "posted Answer",
+    })
+  } catch (error) {
+    next(error)
+  }
 }
 
 exports.getAnswers = (req, res, next) => {

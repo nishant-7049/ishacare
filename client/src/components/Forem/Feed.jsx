132a30ad73659bf59164da7ea7b4808c3ffcd437
Himanshu Rathore <himanshu.rathore2561@gmail.com>
2023-03-18 15:41:47 +0530

    reverse feed

--- client/src/components/Forem/Feed.jsx
+++ client/src/components/Forem/Feed.jsx
@@ -6,9 +6,12 @@ const Feed = ({ data }) => {
   return (
     <div className=' bg-gray-200 flex flex-col gap-2 flex-auto '>
       <ForemBox />
-      {data.map((item) => {
-        return <ForemPost key={item._id} item={item} />
-      })}
+      {data
+        .slice(0)
+        .reverse()
+        .map((item) => {
+          return <ForemPost key={item._id} item={item} />
+        })}
     </div>
   )
 }

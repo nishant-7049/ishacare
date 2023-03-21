82a694d4daecd09241fce1309931369ac1451e28
nishant-7049 <nishantsharmay2@gmail.com>
2023-03-21 19:59:27 +0530

    fixes

--- client/src/pages/BlogPage.jsx
+++ client/src/pages/BlogPage.jsx
@@ -182,8 +182,6 @@ const Container = styled.div`
       align-items: flex-left;
       justify-content: space-between;
       padding: 2rem;
-      background: black;
-      opacity: 0.5;
       > span {
         font-size: 0.8rem;
         color: #50acfb;

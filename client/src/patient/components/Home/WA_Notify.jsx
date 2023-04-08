19d612a03156f8ba9945ff65760a408f84039fc2
Himanshu Rathore <himanshu.rathore2561@gmail.com>
2023-04-08 17:48:53 +0530

    commit

--- client/src/patient/components/Home/WA_Notify.jsx
+++ client/src/patient/components/Home/WA_Notify.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import { ImWhatsapp } from 'react-icons/im'
+import { FiPhoneCall } from 'react-icons/fi'
 
 const WA_Notify = () => {
   return (
@@ -16,6 +17,21 @@ const WA_Notify = () => {
         <a href='https://wa.me/+917383677661' target='_blank'>
           <span className='relative text-[#1ad03f] text-4xl z-10'>
             <ImWhatsapp />
+            <span className='animate-ping absolute w-3 h-3 rounded-full bg-white top-0 left-0'></span>
+          </span>
+        </a>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: 200 }}
+        animate={{
+          opacity: 1,
+          x: 0,
+          transition: { type: 'spring', duration: 1, bounce: 0.5 },
+        }}
+      >
+        <a href='tel:+917383677661'>
+          <span className='relative text-[#84adea] text-4xl z-10'>
+            <FiPhoneCall />
             <span className='animate-ping absolute w-3 h-3 rounded-full bg-white top-0 left-0'></span>
           </span>
         </a>
